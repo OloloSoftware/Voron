b09fb920f60b17c949378dc5435e75a9fe3867ff
AleshaVorontsov <honeycuke@gmail.com>
2016-04-18 01:16:37 +0400

    111

---
+++ TypeScriptHTMLApp2/app.js
@@ -0,0 +1,67 @@
+var context = new AudioContext();
+var gainNode, source, destination, osc, filterNode, convolverNode;
+var analyser;
+analyser = context.createAnalyser();
+analyser.fftSize = 32;
+var fFrequencyData = new Float32Array(analyser.frequencyBinCount);
+var bFrequencyData = new Uint8Array(analyser.frequencyBinCount);
+var bTimeData = new Uint8Array(analyser.frequencyBinCount);
+function play() {
+    osc = context.createOscillator();
+    gainNode = context.createGain();
+    filterNode = context.createBiquadFilter();
+    osc.connect(filterNode);
+    osc.connect(analyser);
+    osc.connect(gainNode);
+    gainNode.connect(context.destination);
+    analyser.connect(context.destination);
+    filterNode.connect(context.destination);
+    osc.start();
+    printFrequency();
+}
+function printFrequency() {
+    // analyser.getFloatFrequencyData(fFrequencyData);
+    analyser.getByteFrequencyData(bFrequencyData);
+    // analyser.getByteTimeDomainData(bTimeData);
+    var freq;
+    freq = document.getElementById('freq');
+    freq.textContent = "1";
+    freq.textContent = bFrequencyData.toString();
+}
+function stop() {
+    osc.stop();
+}
+function changeParam(paramName, target) {
+    osc[paramName].value = target.value;
+    printFrequency();
+}
+function changeGain(target) {
+    gainNode.gain.value = target.value;
+}
+function changeFilter(target) {
+    if (target.value == 1) {
+        filterNode.type = 1;
+        filterNode.frequency.value = 1000;
+        filterNode.frequency.Q = 1;
+    }
+    else if (target.value == 2) {
+        filterNode.type = 2;
+        filterNode.frequency.value = 150;
+        filterNode.frequency.Q = 10;
+    }
+}
+function changeFilter2(target) {
+    if (target.value == 1) {
+        filterNode.type = 1;
+        filterNode.frequency.value = 440;
+        filterNode.frequency.Q = 0;
+    }
+    else if (target.value == 2) {
+        filterNode.type = 2;
+        filterNode.frequency.value = 150;
+        filterNode.frequency.Q = 10;
+    }
+    else
+        filterNode.frequency.value = 0;
+}
+//# sourceMappingURL=app.js.map
